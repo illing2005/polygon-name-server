2495a5b13f8741ace9b2f949acbe0861cd67b668
Sebastian Illing <sebastian@alpaca.casa>
2022-02-23 11:31:11 +0100

    Adds readme

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # Polygon Nameserver (PNS)
 
+This is a simple name server service for the Polygon Network.
+Users can mint domains (.matic) which resemble are human readable name of their wallet address.
+
+The smart contract could be extended to store more information like email, profile picture, ...
+
+## Deployment:
+### Frontend:
+The frontend is deployed using github pages:
+
+https://illing2005.github.io/polygon-name-server/
+
+### Contracts
+The smart contracts are deployed to the mumbai test network:
+
+Domains: [0x87b2a28Cb7AB3C59D7dA867DE9B7521E7a2E61FE](https://mumbai.polygonscan.com/address/0x87b2a28Cb7AB3C59D7dA867DE9B7521E7a2E61FE)
+
+## Features
+### Frontend
+
+- users mint their domain name
+- uses OpenSea API to display already minted tokens
+
+### Solidity Contracts
+- implement the ERC721 token standard.
+- uses on chain metadata storage
+
--- scripts/run.js
+++ scripts/run.js
@@ -1,7 +1,7 @@
 const main = async () => {
   const [owner, superCoder] = await hre.ethers.getSigners();
   const domainContractFactory = await hre.ethers.getContractFactory('Domains');
-  const domainContract = await domainContractFactory.deploy("ninja");
+  const domainContract = await domainContractFactory.deploy("matic");
   await domainContract.deployed();
 
   console.log("Contract owner:", owner.address);
@@ -14,28 +14,12 @@ const main = async () => {
   const balance = await hre.ethers.provider.getBalance(domainContract.address);
   console.log("Contract balance:", hre.ethers.utils.formatEther(balance));
 
-  // Quick! Grab the funds from the contract! (as superCoder)
-  try {
-    txn = await domainContract.connect(superCoder).withdraw();
-    await txn.wait();
-  } catch(error){
-    console.log("Could not rob contract");
-  }
-
-  // Let's look in their wallet so we can compare later
-  let ownerBalance = await hre.ethers.provider.getBalance(owner.address);
-  console.log("Balance of owner before withdrawal:", hre.ethers.utils.formatEther(ownerBalance));
-
-  // Oops, looks like the owner is saving their money!
-  txn = await domainContract.connect(owner).withdraw();
-  await txn.wait();
-
-  // Fetch balance of contract & owner
-  const contractBalance = await hre.ethers.provider.getBalance(domainContract.address);
-  ownerBalance = await hre.ethers.provider.getBalance(owner.address);
-
-  console.log("Contract balance after withdrawal:", hre.ethers.utils.formatEther(contractBalance));
-  console.log("Balance of owner after withdrawal:", hre.ethers.utils.formatEther(ownerBalance));
+  const ownerAddress = await domainContract.getRecordAddress("a16z");
+  console.log(ownerAddress)
+  const domainOwner = await domainContract.getDomainOwner("a16z");
+  console.log(ownerAddress)
+  console.log(await domainContract.getOwnedDomains())
+  console.log(await domainContract.getAllNames())
 }
 
 const runMain = async () => {
